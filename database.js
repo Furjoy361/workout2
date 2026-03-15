4722aed11e1f76fd82c5f2f52e67eba7c8b56c47
yachang hassang <yrhassang@gmail.com>
2026-03-15 09:41:08 -0500

    Add createUser function and update addSquats

--- database.js
+++ database.js
@@ -2,17 +2,48 @@ import { db } from "./firebase.js";
 
 import {
   doc,
-  setDoc
+  setDoc,
+  getDoc
 } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
+
+// -------------------- CREATE USER --------------------
+export async function createUser(user){
+
+  const userRef = doc(db,"users",user.uid);
+
+  const docSnap = await getDoc(userRef);
+
+  // If user does not exist, create it
+  if(!docSnap.exists()){
+
+    await setDoc(userRef,{
+
+      name: user.displayName,
+      pushups: 0,
+      squats: 0,
+      plank: 0
+
+    });
+
+    console.log("User profile created");
+
+  }
+
+}
+
+
+// -------------------- ADD SQUATS --------------------
 export async function addSquats(uid, reps){
 
   const userRef = doc(db,"users",uid);
 
   await setDoc(userRef,{
-    squats: reps
-  });
 
-  console.log("Database write successful");
+    squats: reps
+
+  }, { merge: true });   // important so other fields stay
+
+  console.log("Squats updated successfully");
 
 }
