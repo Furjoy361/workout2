29d3515306c89033e0ba8443c07a8120a81adaa2
yachang hassang <yrhassang@gmail.com>
2026-03-15 10:00:32 -0500

    Update database.js

--- database.js
+++ database.js
@@ -1,38 +1,20 @@
 import { db } from "./firebase.js";
+import { doc, setDoc, increment } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
-import {
-doc,
-setDoc,
-getDoc
-} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+/**
+ * Add squats for a user and save their name if not already present
+ * @param {string} uid - Firebase user UID
+ * @param {number} reps - Number of squats completed
+ * @param {string} name - User's display name
+ */
+export async function addSquats(uid, reps, name) {
 
-export async function addSquats(uid, reps, name){
+  const userRef = doc(db, "users", uid);
 
-const userRef = doc(db,"users",uid);
+  await setDoc(userRef, {
+    squats: increment(reps),
+    name: name
+  }, { merge: true }); // ✅ merge ensures we don't overwrite the entire document
 
-const docSnap = await getDoc(userRef);
-
-if(docSnap.exists()){
-
-const data = docSnap.data();
-
-await setDoc(userRef,{
-name: name,
-squats: (data.squats || 0) + reps
-},{merge:true});
-
-}
-else{
-
-await setDoc(userRef,{
-name: name,
-squats: reps,
-pushups: 0,
-plank: 0
-});
-
-}
-
-console.log("Database write successful");
-
+  console.log("Squats and name saved to database");
 }
