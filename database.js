cf160c35314e5aa53b27de16f430739c68a350f1
yachang hassang <yrhassang@gmail.com>
2026-03-15 03:41:09 -0500

    Update database.js

--- database.js
+++ database.js
@@ -2,8 +2,7 @@ import { db } from "./firebase.js";
 
 import {
   doc,
-  setDoc,
-  increment
+  setDoc
 } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 export async function addSquats(uid, reps){
@@ -11,9 +10,9 @@ export async function addSquats(uid, reps){
   const userRef = doc(db,"users",uid);
 
   await setDoc(userRef,{
-    squats: increment(reps)
-  },{ merge:true });
+    squats: reps
+  });
 
-  console.log("Squats updated");
+  console.log("Database write successful");
 
 }
